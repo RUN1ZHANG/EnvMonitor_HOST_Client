fdbaacfb7fcdc80976f0f117502d1d8436040c46
zhangrun <zrun258@icloud.com>
2025-03-26 23:10:20 +0800

    better_view

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def receive_data():
     global latest_data
     latest_data = request.json  # 解析 JSON 数据
     data = request.json
-    data = {"temp":24.10,"hum":39.00,"sound":2.39,"light":2.47,"air_quality":20,"pir":1,"acc":[-1.20,5.29,8.10],"gyro":[0.03,-0.02,0.09]}
+    #data = {"temp":24.10,"hum":39.00,"sound":2.39,"light":2.47,"air_quality":20,"pir":1,"acc":[-1.20,5.29,8.10],"gyro":[0.03,-0.02,0.09]}
     data["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 添加数据更新时间
     latest_data = data
     print("收到数据:",latest_data)
